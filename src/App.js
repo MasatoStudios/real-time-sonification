8bf7f254ab32ccaf0a6469e13e7985b38c74ea89
Matthew Storey <mattstorey@hotmail.co.uk>
2023-02-13 17:16:30 +0000

    added WebSocket functionality

--- src/App.js
+++ src/App.js
@@ -5,20 +5,56 @@ import {
 	ChakraProvider,
 	Center,
 } from '@chakra-ui/react'
+
 import * as Tone from 'tone'
 import { useState, useEffect } from 'react'
-
+import protobuf from 'protobufjs'
 import Template from './Components/Template/Template'
 import './App.css'
 import Card from './Components/Card/Card'
 import VooClosing from './Data/VooClosing'
 import { PlaySound, StopSound } from './Audio/Noise'
+const { Buffer } = require('buffer/');
+
+function formatPrice(price){
+	return `$${price.toFixed(2)}`
+}
+
 
 // More research https://reactronica.com/
 // May include instead of Tone.js
 
 const App = () => {
 	const [playing, setPlaying] = useState(false)
+	const [current, setCurrent] = useState(null)
+
+	useEffect(() =>{
+		const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+
+		protobuf.load('./YPricingData.proto', (error, root) => {
+			if(error){
+				return console.log(error)
+			}
+			const Yaticker = root.lookupType('yaticker')
+
+			ws.onopen = function open() {
+  				console.log('connected');
+  				ws.send(JSON.stringify({
+    			subscribe: ['VOO']
+  				}));
+			};
+
+			ws.onclose = function close() {
+				console.log('disconnected');
+			};
+
+			ws.onmessage = function incoming(message) {
+				const next = (Yaticker.decode(new Buffer(message.data, 'base64')))
+				setCurrent(next)
+				};
+			})
+
+	},[])
 
 	useEffect(() => {
 		if (playing === true) {
@@ -36,7 +72,7 @@ const App = () => {
 						<Card
 							Name='Vanguard S&P 500'
 							Ticker='VOO'
-							Price='1234'
+							Price={current && <>{formatPrice(current.price)}</>}
 							Opening='3900.97'
 						/>
 						<Card
