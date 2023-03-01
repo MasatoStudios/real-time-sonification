5668b0a664e356376a10652500940e03b87c7f8f
Matthew Storey <mattstorey@hotmail.co.uk>
2023-03-01 21:54:07 +0000

    added alarm and filter change functions

--- src/Audio/Noise.js
+++ src/Audio/Noise.js
@@ -2,7 +2,7 @@ import * as Tone from 'tone'
 
 const noise = new Tone.Noise('brown')
 // lower the volume to -40db
-noise.volume.value = -40
+noise.volume.value = -35
 const noiseFilter = new Tone.Filter(1000, 'lowpass').connect(Tone.Master)
 noise.connect(noiseFilter)
 
@@ -15,4 +15,42 @@ export function PlaySound() {
 export function StopSound() {
 	Tone.Master.mute = true
 	console.log('Stopping audio now')
+}
+
+export function DropAlarm() {
+	//play a sound when price drops
+	const lowAlarm = new Tone.Synth().toDestination()
+	const now = Tone.now()
+
+	lowAlarm.triggerAttack('A3', now)
+	lowAlarm.triggerRelease(now + 0.5)
+}
+
+export function RiseAlarm() {
+	//play a sound when price rises
+	const highAlarm = new Tone.Synth().toDestination()
+	const now = Tone.now()
+
+	highAlarm.triggerAttackRelease('A6', now)
+	highAlarm.triggerRelease(now + 0.5)
+}
+
+export function FilterChange(ref) {
+	if (ref < 0) {
+		noiseFilter.frequency.value -= 10
+		// console.log('adding to filter')
+		// console.log(noiseFilter.frequency.value);
+		if (noiseFilter.frequency.value < 50) {
+			noiseFilter.frequency.value = 1000
+			return 0
+		}
+	} else if (ref > 0) {
+		noiseFilter.frequency.value += 10
+		// console.log('decreasing filter')
+		// console.log(noiseFilter.frequency.value);
+		if (noiseFilter.frequency.value >= 3500) {
+			noiseFilter.frequency.value = 1000
+			return 0
+		}
+	}
 }
