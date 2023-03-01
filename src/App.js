d05198529f0c548132da72de8e988e6ed35bceb6
Matthew Storey <mattstorey@hotmail.co.uk>
2023-03-01 21:53:45 +0000

    refactored added function for each stock

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import {
 	Flex,
 	Button,
@@ -5,56 +6,71 @@ import {
 	ChakraProvider,
 	Center,
 } from '@chakra-ui/react'
-
 import * as Tone from 'tone'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import protobuf from 'protobufjs'
 import Template from './Components/Template/Template'
 import './App.css'
 import Card from './Components/Card/Card'
 import VooClosing from './Data/VooClosing'
-import { PlaySound, StopSound } from './Audio/Noise'
-const { Buffer } = require('buffer/');
+import {
+	PlaySound,
+	StopSound,
+	DropAlarm,
+	RiseAlarm,
+	FilterChange,
+} from './Audio/Noise'
+import useStock from './Components/Hooks/useStock'
+const { Buffer } = require('buffer/')
 
-function formatPrice(price){
+function formatPrice(price) {
 	return `$${price.toFixed(2)}`
 }
-
 
 // More research https://reactronica.com/
 // May include instead of Tone.js
 
 const App = () => {
 	const [playing, setPlaying] = useState(false)
-	const [current, setCurrent] = useState(null)
+	const {
+		current: currentVOO,
+		changePercent: changePercentVOO,
+		openPrice: openPriceVOO,
+		oldPrice: oldPriceVOO,
+	} = useStock('VOO')
+	const {
+		current: currentJEPQ,
+		changePercent: changePercentJEPQ,
+		openPrice: openPriceJEPQ,
+		oldPrice: oldPriceJEPQ,
+	} = useStock('JEPQ')
+	const {
+		current: currentARKK,
+		changePercent: changePercentARKK,
+		openPrice: openPriceARKK,
+		oldPrice: olderPriceARKK,
+	} = useStock('ARKK')
 
-	useEffect(() =>{
-		const ws = new WebSocket('wss://streamer.finance.yahoo.com');
+	const {
+		current: currentFB,
+		changePercent: changePercentFB,
+		openPrice: openPriceFB,
+		oldPrice: oldPriceFB,
+	} = useStock('FB')
 
-		protobuf.load('./YPricingData.proto', (error, root) => {
-			if(error){
-				return console.log(error)
-			}
-			const Yaticker = root.lookupType('yaticker')
+	const {
+		current: currentNVDA,
+		changePercent: changePercentNVDA,
+		openPrice: openPriceNVDA,
+		oldPrice: oldPriceNVDA,
+	} = useStock('NVDA')
 
-			ws.onopen = function open() {
-  				console.log('connected');
-  				ws.send(JSON.stringify({
-    			subscribe: ['VOO']
-  				}));
-			};
-
-			ws.onclose = function close() {
-				console.log('disconnected');
-			};
-
-			ws.onmessage = function incoming(message) {
-				const next = (Yaticker.decode(new Buffer(message.data, 'base64')))
-				setCurrent(next)
-				};
-			})
-
-	},[])
+	const {
+		current: currentAAPL,
+		changePercent: changePercentAAPL,
+		openPrice: openPriceAAPL,
+		oldPrice: oldPriceAAPL,
+	} = useStock('AAPL')
 
 	useEffect(() => {
 		if (playing === true) {
@@ -62,6 +78,22 @@ const App = () => {
 		} else if (playing === false) {
 			StopSound()
 		}
+		//make am alarm if price drops above/below 1% of old price
+		if (changePercentVOO.current < -1) {
+			console.log('drop')
+			// console.log(changePercent.current)
+			DropAlarm()
+		} else if (changePercentVOO.current > 1) {
+			RiseAlarm()
+			console.log('rise')
+			// console.log(changePercent.current)
+		}
+
+		//Change the filter depending on the trend of the stock
+		FilterChange(changePercentVOO.current)
+
+		// troubleshooting for changePercent Function
+		// console.log(changePercent.current)
 	})
 
 	return (
@@ -72,40 +104,44 @@ const App = () => {
 						<Card
 							Name='Vanguard S&P 500'
 							Ticker='VOO'
-							Price={current && <>{formatPrice(current.price)}</>}
-							Opening='3900.97'
+							Price={currentVOO && <>{formatPrice(currentVOO.price)}</>}
+							Opening={openPriceVOO && <>{formatPrice(openPriceVOO.current)}</>}
 						/>
 						<Card
-							Name='S&P 500'
-							Ticker='SPX'
-							Price='4000.85'
-							Opening='3900.97'
+							Name='JPMorgan Nasdaq ETF'
+							Ticker='JEPQ'
+							Price={currentJEPQ && <>{formatPrice(currentJEPQ.price)}</>}
+							Opening={
+								openPriceJEPQ && <>{formatPrice(openPriceJEPQ.current)}</>
+							}
 						/>
 						<Card
-							Name='S&P 500'
-							Ticker='SPX'
-							Price='4000.85'
-							Opening='3900.97'
+							Name='ARK Innovation ETF'
+							Ticker='ARKK'
+							Price={currentARKK && <>{formatPrice(currentARKK.price)}</>}
+							Opening={
+								openPriceARKK && <>{formatPrice(openPriceARKK.current)}</>
+							}
 						/>
 					</Flex>
 					<Flex className='container'>
 						<Card
-							Name='S&P 500'
-							Ticker='SPX'
-							Price='4000.85'
-							Opening='3900.97'
+							Name='Facebook'
+							Ticker='FB'
+							Price={currentFB && <>{formatPrice(currentFB.price)}</>}
+							Opening={currentFB && <>{formatPrice(openPriceFB.current)}</>}
 						/>
 						<Card
-							Name='S&P 500'
-							Ticker='SPX'
-							Price='4000.85'
-							Opening='3900.97'
+							Name='Nvidia'
+							Ticker='NVDA'
+							Price={currentNVDA && <>{formatPrice(currentNVDA.price)}</>}
+							Opening={currentNVDA && <>{formatPrice(openPriceNVDA.current)}</>}
 						/>
 						<Card
-							Name='S&P 500'
-							Ticker='SPX'
-							Price='4000.85'
-							Opening='3900.97'
+							Name='Apple'
+							Ticker='AAPL'
+							Price={currentAAPL && <>{formatPrice(currentAAPL.price)}</>}
+							Opening={currentAAPL && <>{formatPrice(openPriceAAPL.current)}</>}
 						/>
 					</Flex>
 				</div>
@@ -125,7 +161,6 @@ const App = () => {
 						Stop
 					</Button>
 				</ButtonGroup>
-
 				{/* <VooClosing /> */}
 			</Template>
 		</ChakraProvider>
