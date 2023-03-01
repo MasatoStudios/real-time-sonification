5ca1010c1c285a736d3fc74afe89ad1aeb3409a6
Matthew Storey <mattstorey@hotmail.co.uk>
2023-03-01 21:55:26 +0000

    added classNames for styling

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -5,18 +5,18 @@ const Card = ({ Name, Ticker, Price, Opening }) => {
 		<div className='card'>
 			<div className='cardHeader'>
 				<div>
-					<h1>{Ticker}</h1>
+					<h1 className='ticker'>{Ticker}</h1>
 				</div>
 				<div>
-					<h2>{Name}</h2>
+					<h2 className='title'>{Name}</h2>
 				</div>
 			</div>
 			<div className='cardBody'>
 				<div>
-					<h2> Price: {Price} </h2>
+					<h2 className='price'> Price: {Price} </h2>
 				</div>
 				<div>
-					<h3> Opening Price: {Opening}</h3>
+					<h3 className='opening'> Opening Price: {Opening}</h3>
 				</div>
 			</div>
 		</div>
